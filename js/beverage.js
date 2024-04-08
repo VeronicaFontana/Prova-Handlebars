1feec3d20ca9a6e2fb8e16ee3a40e67b360fbdfb
Veronica Fontana <veronyc98@hotmail.it>
2024-04-08 11:59:05 +0200

    [add] Dolci & beverage lists, UL in Menu page

---
+++ js/beverage.js
@@ -0,0 +1,22 @@
+const bibite = [
+    {
+        nome: "Acqua naturale/frizzante - bottiglia 0.5l",
+        prezzo: "1.50"
+    },
+    {
+        nome: "Coca cola/coca cola zero - bottiglia 0.5l",
+        prezzo: "3"
+    },
+    {
+        nome: "Thè alla pesca/limone - bottiglia 0.5l",
+        prezzo: "3.50"
+    },
+    {
+        nome: "Fanta - bottiglia 0.5l",
+        prezzo: "3"
+    },
+    {
+        nome: "Birra - 0.33",
+        prezzo: "3"
+    }
+]
---
+++ js/dolci.js
@@ -0,0 +1,26 @@
+const dolci = [
+    {
+        nome: "Tiramisu",
+        prezzo: "5"
+    },
+    {
+        nome: "Bignè ripieno",
+        prezzo: "3"
+    },
+    {
+        nome: "Torta della nonna",
+        prezzo: "6"
+    },
+    {
+        nome: "Muffin al cioccolato",
+        prezzo: "4"
+    },
+    {
+        nome: "Donut allo zucchero",
+        prezzo: "3"
+    },
+    {
+        nome: "Donut al cioccolato",
+        prezzo: "4"
+    }
+]
